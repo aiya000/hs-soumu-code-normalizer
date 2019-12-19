23569da4eee771f83c66919d11b93f52b7510609
aiya000 <aiya000.develop@gmail.com>
2019-12-19 18:38:20 +0900

    :tada: Implement converting from .xlsx to .json

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,15 @@ module Main where
 import Safe
 import Soumu.Main (readToNormalize)
 import System.Environment (getArgs)
+import TextShow (printT)
 
 main :: IO ()
 main = do
   maybeXls <- headMay <$> getArgs
   case maybeXls of
     Nothing -> putStrLn "expected an argument as a .xls, because no arguments was taken."
-    Just xls -> readToNormalize xls >>= print
+    Just xls -> do
+      contentOrErr <- readToNormalize xls
+      case contentOrErr of
+        Left e -> print e
+        Right content -> printT content
--- src/Soumu/Main.hs
+++ src/Soumu/Main.hs
@@ -3,13 +3,17 @@ module Soumu.Main where
 import Codec.Xlsx
 import qualified Codec.Xlsx as Xlsx
 import Control.Lens
-import Control.Monad.Except (ExceptT (..), runExceptT)
+import Control.Monad.Except (ExceptT (..), runExceptT, throwError)
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson (ToJSON)
 import qualified Data.ByteString.Lazy as ByteString
+import Data.Maybe (catMaybes)
 import Data.Monoid ((<>))
 import Data.Text (Text)
-import GHC.Generics
+import qualified Data.Text as Text
+import GHC.Generics hiding (to)
+import qualified TextShow as TextShow
+import TextShow (TextShow (..))
 
 type Result = [Row]
 
@@ -24,16 +28,64 @@ data Row = Row
   deriving (Show, Eq, Generic)
   deriving anyclass (ToJSON)
 
+instance TextShow Row where
+  showb Row{..} =
+    "{\"code\":\"" <> TextShow.fromText code <>
+    "\",\"prefeacture\":\"" <> TextShow.fromText prefeacture <>
+    "\",\"city\":\"" <> TextShow.fromText city <>
+    "\",\"prefeactureKana\":\"" <> TextShow.fromText prefeactureKana <>
+    "\",\"cityKana\":\"" <> TextShow.fromText cityKana <>
+    "\"}"
+
+mainSheetName :: Text
+mainSheetName = "R1.5.1現在の団体"
+
+columnOfCode :: Int
+columnOfCode = 1
+
+columnOfPrefecture :: Int
+columnOfPrefecture = 2
+
+columnOfCity :: Int
+columnOfCity = 3
+
+columnOfPrefectureKana :: Int
+columnOfPrefectureKana = 4
+
+columnOfCityKana :: Int
+columnOfCityKana = 5
+
+validTableRowRange :: [Int]
+validTableRowRange = [2 .. 5000]
+
 readToNormalize :: FilePath -> IO (Either Error Result)
 readToNormalize xlsFile = runExceptT do
   xlsContent <- liftIO $ ByteString.readFile xlsFile
   xls <- ExceptT . pure . meaningless $ toXlsxEither xlsContent
-  liftIO . print $ xls ^? ixSheet "R1.5.1現在の団体"
-  pure undefined
+  sheet <- rejectNothing $ xls ^? ixSheet mainSheetName
+  let valuesMaybeRow = map (rowToRow sheet) validTableRowRange
+  pure $ catMaybes valuesMaybeRow
+  where
+    rejectNothing :: Maybe a -> ExceptT Error IO a
+    rejectNothing Nothing  = throwError $ "No sheet '" <> mainSheetName <> "' found."
+    rejectNothing (Just x) = pure x
 
+-- | Converts A plain row to Result's row
+rowToRow :: Worksheet -> Int -> Maybe Row
+rowToRow sheet rowNum =
+  Row <$>
+    sheet ^. cellValueAt (rowNum, columnOfCode)           . _Just . to textCell <*>
+    sheet ^. cellValueAt (rowNum, columnOfPrefecture)     . _Just . to textCell <*>
+    sheet ^. cellValueAt (rowNum, columnOfCity)           . _Just . to textCell <*>
+    sheet ^. cellValueAt (rowNum, columnOfPrefectureKana) . _Just . to textCell <*>
+    sheet ^. cellValueAt (rowNum, columnOfCityKana)       . _Just . to textCell
+  where
+    textCell :: CellValue -> Maybe Text
+    textCell (CellText x) = Just x
+    textCell _ = Nothing
 
-type Error = String
+type Error = Text
 
 meaningless :: Either Xlsx.ParseError a -> Either Error a
 meaningless (Right x) = Right x
-meaningless (Left x) = Left $ "error! " <> show x
+meaningless (Left x) = Left . Text.pack $ "error! " <> show x
